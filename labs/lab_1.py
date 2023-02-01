ccb26c092881715dc2d00561ea121a4f4b38a775
Radley Ciego <radleykc@gmail.com>
2023-01-31 21:11:27 -0500

    Create lab_1.py

---
+++ labs/lab_1.py
@@ -0,0 +1,15 @@
+# Radley Ciego
+# Lab 1
+
+# Task 1
+import random
+random.seed(100)
+list = []
+for i in range(0,10): # length of list
+    list.append(random.randint(0,250)) 
+print(list)
+
+def mean(list):
+    return sum(list)/len(list)
+
+print(mean(list))
