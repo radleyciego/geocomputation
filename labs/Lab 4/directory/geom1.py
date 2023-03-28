0b8555b9249d0b918f41e52be3f43ab3d9e5a60e
Radley Ciego <radleykc@gmail.com>
2023-03-28 16:56:54 -0400

    Update geom1.py

--- labs/Lab 4/directory/geom1.py
+++ labs/Lab 4/directory/geom1.py
@@ -3,17 +3,31 @@ import random
 
 class Geom():
     geomType = 'Generic Geometry Type'
+
     def __init__(self):
         self.name = random.choice(['Sam','Pedro','Carlos','Maria','Khadija'])
         self.color = random.choice(['BLUE','RED','ORANGE','GREEN'])
+
     def print_name(self):
         print('My name is ',self.name, 'and my color is ',self.color)
+
     def makeString(self):
         return f"Name: {self.name}, Color: {self.color}, Area: {self.area()}"
 
-# constructior of the class: used to initialize an object
-
-
+class Circle(Geom):
+  
+  def __init__ (self, r):
+    self.radius = r
+    super().__init__()
+  def area(self):
+     return mth.pi * self.radius **2
+  
+class Square(Geom):
+  def __init__ (self, s):
+    self.side = s
+    super().__init__()
+  def area(self):
+     return self.side **2
 
 class Triangle(Geom):
     def __init__(self, a, b, c):
