6ba025e514f82a420eb7bdc5587102f217cfe3a6
Radley Ciego <radleykc@gmail.com>
2023-03-28 08:39:45 -0400

    Update triangle.py

--- labs/Lab 4/directory/triangle.py
+++ labs/Lab 4/directory/triangle.py
@@ -1,6 +1,12 @@
+class Geom():
+    geomType = 'Generic Geometry Type'
+
 class Triangle(Geom):
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
         super().__init__()
+        
+    def area(self):
+        return (self.a + self.b + self.c)** 2
