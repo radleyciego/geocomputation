af030c5c1bf9d033a3fe83dbee61818c21e30647
Radley Ciego <radleykc@gmail.com>
2023-03-28 08:39:42 -0400

    Create geom1.py

---
+++ labs/Lab 4/directory/geom1.py
@@ -0,0 +1,25 @@
+import math as mth
+import random
+
+class Geom():
+    geomType = 'Generic Geometry Type'
+    def __init__(self):
+        self.name = random.choice(['Sam','Pedro','Carlos','Maria','Khadija'])
+        self.color = random.choice(['BLUE','RED','ORANGE','GREEN'])
+    def print_name(self):
+        print('My name is ',self.name, 'and my color is ',self.color)
+    def makeString(self):
+        return f"Name: {self.name}, Color: {self.color}, Area: {self.area()}"
+
+# constructior of the class: used to initialize an object
+
+
+
+class Triangle(Geom):
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        super().__init__()
+    def area(self):
+        return (self.a + self.b + self.c)** 2
